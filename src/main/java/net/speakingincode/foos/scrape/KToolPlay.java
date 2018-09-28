d99ba201741f67aae5a3fef8d065690d0d3e2ce1
speakingincode <brian@speakingincode.net>
2018-09-28 08:32:24 -0700

    Fix bug with no third-place match played

--- src/main/java/net/speakingincode/foos/scrape/KToolPlay.java
+++ src/main/java/net/speakingincode/foos/scrape/KToolPlay.java
@@ -9,60 +9,67 @@ import java.util.List;
 
 @AutoValue
 public abstract class KToolPlay {
-    abstract boolean valid();
-    public abstract Team team1();
-    public abstract @Nullable Team team2();
-    public abstract @Nullable List<Match> disciplines();
+  abstract boolean valid();
 
-    public boolean matchWasPlayed() {
-        return valid() && team2() != null;
+  public abstract @Nullable
+  Team team1();
+
+  public abstract @Nullable
+  Team team2();
+
+  public abstract @Nullable
+  List<Match> disciplines();
+
+  public boolean matchWasPlayed() {
+    return valid() && team2() != null;
+  }
+
+  public static KToolPlay create(boolean valid, Team t1, Team t2, List<Match> disciplines) {
+    return new AutoValue_KToolPlay(valid, t1, t2, disciplines);
+  }
+
+  public static TypeAdapter<KToolPlay> typeAdapter(Gson gson) {
+    return new AutoValue_KToolPlay.GsonTypeAdapter(gson);
+  }
+
+  @AutoValue
+  public abstract static class Team {
+    public abstract String id();
+
+    public static Team create(String id) {
+      return new AutoValue_KToolPlay_Team(id);
     }
 
-    public static KToolPlay create(boolean valid, Team t1, Team t2, List<Match> disciplines) {
-        return new AutoValue_KToolPlay(valid, t1, t2, disciplines);
+    public static TypeAdapter<Team> typeAdapter(Gson gson) {
+      return new AutoValue_KToolPlay_Team.GsonTypeAdapter(gson);
+    }
+  }
+
+  @AutoValue
+  public abstract static class Match {
+    public abstract List<Set> sets();
+
+    public static Match create(List<Set> sets) {
+      return new AutoValue_KToolPlay_Match(sets);
     }
 
-    public static TypeAdapter<KToolPlay> typeAdapter(Gson gson) {
-        return new AutoValue_KToolPlay.GsonTypeAdapter(gson);
+    public static TypeAdapter<Match> typeAdapter(Gson gson) {
+      return new AutoValue_KToolPlay_Match.GsonTypeAdapter(gson);
+    }
+  }
+
+  @AutoValue
+  public abstract static class Set {
+    public abstract int team1();
+
+    public abstract int team2();
+
+    public static Set create(int team1, int team2) {
+      return new AutoValue_KToolPlay_Set(team1, team2);
     }
 
-    @AutoValue
-    public abstract static class Team {
-        public abstract String id();
-
-        public static Team create(String id) {
-            return new AutoValue_KToolPlay_Team(id);
-        }
-
-        public static TypeAdapter<Team> typeAdapter(Gson gson) {
-            return new AutoValue_KToolPlay_Team.GsonTypeAdapter(gson);
-        }
+    public static TypeAdapter<Set> typeAdapter(Gson gson) {
+      return new AutoValue_KToolPlay_Set.GsonTypeAdapter(gson);
     }
-
-    @AutoValue
-    public abstract static class Match {
-        public abstract List<Set> sets();
-
-        public static Match create(List<Set> sets) {
-            return new AutoValue_KToolPlay_Match(sets);
-        }
-
-        public static TypeAdapter<Match> typeAdapter(Gson gson) {
-            return new AutoValue_KToolPlay_Match.GsonTypeAdapter(gson);
-        }
-    }
-
-    @AutoValue
-    public abstract static class Set {
-        public abstract int team1();
-        public abstract int team2();
-
-        public static Set create(int team1, int team2) {
-            return new AutoValue_KToolPlay_Set(team1, team2);
-        }
-
-        public static TypeAdapter<Set> typeAdapter(Gson gson) {
-            return new AutoValue_KToolPlay_Set.GsonTypeAdapter(gson);
-        }
-    }
+  }
 }
--- src/test/java/net/speakingincode/foos/scrape/KToolResultsTest.java
+++ src/test/java/net/speakingincode/foos/scrape/KToolResultsTest.java
@@ -13,51 +13,63 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class KToolResultsTest {
-    private static final ZoneId LA_TIMEZONE = ZoneId.of("America/Los_Angeles");
-    private static final ZoneId ABIDJAN_TIMEZONE = ZoneId.of("Africa/Abidjan");
+  private static final ZoneId LA_TIMEZONE = ZoneId.of("America/Los_Angeles");
+  private static final ZoneId ABIDJAN_TIMEZONE = ZoneId.of("Africa/Abidjan");
 
-    @Test
-    public void dateParseWorks() throws Exception {
-        assertThat(KToolResults.getLocalDate("2018-04-11T03:07:59.255Z", LA_TIMEZONE),
-            equalTo(LocalDate.of(2018, 4, 10)));
-        assertThat(KToolResults.getLocalDate("2018-04-11T03:07:59.255Z", ABIDJAN_TIMEZONE),
-            equalTo(LocalDate.of(2018, 4, 11)));
-        assertThat(KToolResults.getLocalDate("2018-04-10T12:07:59.255Z", LA_TIMEZONE),
-            equalTo(LocalDate.of(2018, 4, 10)));
-        assertThat(KToolResults.getLocalDate("2016-05-11T12:07:59.255Z", LA_TIMEZONE),
-            equalTo(LocalDate.of(2016, 5, 11)));
-    }
+  @Test
+  public void dateParseWorks() throws Exception {
+    assertThat(KToolResults.getLocalDate("2018-04-11T03:07:59.255Z", LA_TIMEZONE),
+        equalTo(LocalDate.of(2018, 4, 10)));
+    assertThat(KToolResults.getLocalDate("2018-04-11T03:07:59.255Z", ABIDJAN_TIMEZONE),
+        equalTo(LocalDate.of(2018, 4, 11)));
+    assertThat(KToolResults.getLocalDate("2018-04-10T12:07:59.255Z", LA_TIMEZONE),
+        equalTo(LocalDate.of(2018, 4, 10)));
+    assertThat(KToolResults.getLocalDate("2016-05-11T12:07:59.255Z", LA_TIMEZONE),
+        equalTo(LocalDate.of(2016, 5, 11)));
+  }
 
-    @Test
-    public void parsesRealResults() throws Exception {
-        try (InputStream testInput = getClass().getResourceAsStream("/example.ktool")) {
-             String json = CharStreams.toString(new InputStreamReader(testInput, Charsets.UTF_8));
-             KToolResults result = KToolResults.fromJson(json);
-             assertThat(result.players().get(0).name(), equalTo("Vera"));
-             assertThat(result.ko().third().plays().get(0).team1().id(),
-                equalTo("c7bbd364-8db4-4238-fdc5-37935966493a"));
-            assertThat(result.plays().get(0).team1().id(),
-                equalTo("fa30aa22-175f-436c-a0f0-7472a3567d81"));
-        }
+  @Test
+  public void parsesRealResults() throws Exception {
+    try (InputStream testInput = getClass().getResourceAsStream("/example.ktool")) {
+      String json = CharStreams.toString(new InputStreamReader(testInput, Charsets.UTF_8));
+      KToolResults result = KToolResults.fromJson(json);
+      assertThat(result.players().get(0).name(), equalTo("Vera"));
+      assertThat(result.ko().third().plays().get(0).team1().id(),
+          equalTo("c7bbd364-8db4-4238-fdc5-37935966493a"));
+      assertThat(result.plays().get(0).team1().id(),
+          equalTo("fa30aa22-175f-436c-a0f0-7472a3567d81"));
     }
+  }
 
-    @Test
-    public void v2format() throws Exception {
-        try (InputStream testInput = getClass().getResourceAsStream("/v2-format.ktool")) {
-            String json = CharStreams.toString(new InputStreamReader(testInput, Charsets.UTF_8));
-            KToolResults result = KToolResults.fromJson(json);
-            assertThat(result.players().get(0).name(), equalTo("Vera"));
-            assertThat(result.plays().get(0).team1().id(), equalTo("SyNgNQNSwWm"));
-            assertThat(result.ko().third().plays().get(0).team1().id(), equalTo("S1sW7ZwvZ7"));
-        }
+  @Test
+  public void v2format() throws Exception {
+    try (InputStream testInput = getClass().getResourceAsStream("/v2-format.ktool")) {
+      String json = CharStreams.toString(new InputStreamReader(testInput, Charsets.UTF_8));
+      KToolResults result = KToolResults.fromJson(json);
+      assertThat(result.players().get(0).name(), equalTo("Vera"));
+      assertThat(result.plays().get(0).team1().id(), equalTo("SyNgNQNSwWm"));
+      assertThat(result.ko().third().plays().get(0).team1().id(), equalTo("S1sW7ZwvZ7"));
     }
+  }
 
-    @Test
-    public void playoffBye() throws Exception {
-        try (InputStream testInput = getClass().getResourceAsStream("/playoff-bye.ktool")) {
-            String json = CharStreams.toString(new InputStreamReader(testInput, Charsets.UTF_8));
-            KToolResults result = KToolResults.fromJson(json);
-            assertThat(result.players().get(0).name(), equalTo("Naveen/Bharath"));
-        }
+  @Test
+  public void playoffBye() throws Exception {
+    try (InputStream testInput = getClass().getResourceAsStream("/playoff-bye.ktool")) {
+      String json = CharStreams.toString(new InputStreamReader(testInput, Charsets.UTF_8));
+      KToolResults result = KToolResults.fromJson(json);
+      assertThat(result.players().get(0).name(), equalTo("Naveen/Bharath"));
     }
+  }
+
+  @Test
+  public void emptyThirdPlace() throws Exception {
+    try (InputStream testInput = getClass().getResourceAsStream("/empty-third-place.ktool")) {
+      String json = CharStreams.toString(new InputStreamReader(testInput, Charsets.UTF_8));
+      KToolResults result = KToolResults.fromJson(json);
+      assertThat(result.players().get(0).name(), equalTo("Lih"));
+      assertThat(result.players().get(1).name(), equalTo("Jorge"));
+      assertThat(result.ko().third().plays().get(0).valid(), equalTo(false));
+      assertThat(result.ko().third().plays().get(0).team1(), equalTo(null));
+    }
+  }
 }
